6ff80cf1d62b1c0d111bda52b4385327e3fc016f
Christian Chamorro Vela <99835009+ChristianChV@users.noreply.github.com>
2024-05-16 21:06:32 -0500

    linter added

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,9 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.klint) apply false
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
